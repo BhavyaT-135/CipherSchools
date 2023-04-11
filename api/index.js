e41f1c9e7821cbc2e2811ed605b429267d212405
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-11 12:18:10 +0530

    🗃️ Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
-dotenv.config()
+dotenv.config({path: "./vars/.env"})
 
 app.use(cors())
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
@@ -13,6 +16,15 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
