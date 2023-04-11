e55a7bc72ae591945c608215138df9eee23f5206
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-11 13:06:25 +0530

    ✨ Added the user model

--- api/index.js
+++ api/index.js
@@ -4,19 +4,25 @@ const connectDB = require('./db/connect')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
+// Routes
 const authRouter = require('./routes/auth')
 
+// Middleware
+const notFoundMiddleware = require('./middleware/not-found');
+
 dotenv.config({path: "./vars/.env"})
-
 app.use(cors())
-
 app.use(express.json());
 
+// Api routes
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
 app.use('/api/v1/auth', authRouter)
+
+// Using the middleware
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 5000
 
---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide username'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'],
+    unique: true
+  },
+  profileImage: {
+    type: String,
+    default: ""
+  },
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
