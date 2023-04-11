a9e45642dcd3de0e04f74b2e930fc58b676f2352
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-11 20:26:43 +0530

    ✨ Image route using multer

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ require('express-async-errors');
 const connectDB = require('./db/connect')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const multer = require('multer');
+const path = require('path');
 
 // Routes
 const authRouter = require('./routes/auth')
@@ -16,10 +18,25 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 dotenv.config({path: "./vars/.env"})
 app.use(cors())
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");        
+    }, filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 // Api routes
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
+})
+
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
 })
 
 app.use('/api/v1/auth', authRouter)
