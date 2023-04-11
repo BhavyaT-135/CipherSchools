d32222cd1a3f57ddad40887a3d8cdf33bfe84467
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-11 20:19:11 +0530

    ✨ Followers controllers created

--- api/controllers/followers.js
+++ api/controllers/followers.js
@@ -1,12 +1,29 @@
-
-
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
 
 const getFollowers = async (req, res) => {
-  res.send(`All the followers of user ${req.params.userId}`)
+  const userId = req.params.userId;
+
+  try {
+    const user = await User.findById(userId).populate('followers');
+    res.status(StatusCodes.OK).json(user.followers);
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error retrieving followers' });
+  }
 }
 
 const addFollowers = async (req, res) => {
-  res.send(`Add a follower to user ${req.params.userId}`)
+  const userId = req.params.userId;
+  const followerId = req.body.followerId;
+
+  try {
+    const user = await User.findByIdAndUpdate(userId, { $addToSet: { followers: followerId } });
+    res.status(StatusCodes.CREATED).json(user.followers);
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error adding follower' });
+  }
 }
 
 module.exports = {
