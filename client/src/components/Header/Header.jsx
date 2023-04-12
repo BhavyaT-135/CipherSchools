621faefc360685b0a04d2f065ba8d95ef1ae0f2c
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-12 14:11:13 +0530

    💄 Header component styling - responsive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './header.css'
 import Toggle from '../Toggle/Toggle'
+import Logo from '../../assets/Logo.png'
+import pf from '../../assets/pf.jpeg'
+import points from '../../assets/points.svg'
 
 const Header = () => {
   return (
     <div className="cipher__header">
       <div className='cipher__header-left'>
         <i className="bi bi-list cipher__header-icon" />
-        <img className='cipher__header-logo' src="" alt='Logo' />
+        <img className='cipher__header-logo' src={Logo} alt='Logo' />
         <div className='cipher__header-title'>CipherSchools</div>
         <div className='cipher__header_browse'>
           <i className="bi bi-compass cipher__header_browse-icon" />
@@ -21,9 +24,9 @@ const Header = () => {
           <input className='cipher__header_input' placeholder='Search and Learn' />
           <i className='bi bi-ui-radios cipher__header_input-icon' />
         </div>
-        <i className='bi bi-bell cipher__header-icon' />
-        <img className='cipher__header-profile' src="" alt='ProfileImage' />
-        <img className='cipher__header-points' src="" alt='LogoPoints' />
+        <i className='bi bi-bell cipher__header-rightIcon' />
+        <img className='cipher__header-profile' src={pf} alt='ProfileImage' />
+        <img className='cipher__header-points' src={points} alt='LogoPoints' />
         <Toggle />
       </div>
     </div>
