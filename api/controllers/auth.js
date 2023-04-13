ed6fc1bc8e2b921c2114f7a7d6586bb5adcf2773
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-13 13:48:45 +0530

    ✨ Patch route for users

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -31,8 +31,29 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({user})
 }
 
+const updateUser = async (req, res) => {
+  const { id: userId } = req.params;
+
+  if (req.body.password) {
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+  }
+
+  const user = await User.findOneAndUpdate({ _id: userId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!user) {
+    return res.status(StatusCodes.NOT_FOUND).json({msg: `No task with id : ${req.params.id}`})
+  }
+
+  res.status(StatusCodes.OK).json({ user })
+}
+
 
 module.exports = {
   register,
-  login
+  login,
+  updateUser
 }
